94f34250a7134be4bef9f181182eec7403c50959
Adriele Primo <adriele.dejprimo@gmail.com>
2023-08-15 17:48:02 -0300

    using interface

--- src/model/services/ContractService.java
+++ src/model/services/ContractService.java
@@ -3,11 +3,10 @@ package model.services;
 import entities.Contract;
 
 public class ContractService {
-	
-	//private OnlinePaymentService paymentService;
+
 	
 	public void processContract(Contract contract, int months) {
-		//paymentService.interest(contract.getTotalValue(), months);
+	
 
 	}
 }
